e0e9b1d6d59b2de6c9bead37c3845a866eea71a1
Theerathat Pornprinya <35303647+theerathat2008@users.noreply.github.com>
2019-07-11 21:52:18 +0700

    Update and rename IndexController.php to SCGController.php

--- SCG/module/Application/src/Controller/SCGController.php
+++ SCG/module/Application/src/Controller/SCGController.php
@@ -10,7 +10,7 @@ namespace Application\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class IndexController extends AbstractActionController
+class SCGController extends AbstractActionController
 {
     public function indexAction()
     {
